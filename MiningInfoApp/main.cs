5ed0d1331482ec61b6384d810c9f1e636063d410
idahomike55 <idahomike55@gmail.com>
2014-03-29 19:29:58 -0600

    should have been in last commit

---
+++ MiningInfoApp/main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiningInfoApp
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void publicToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 newMDIChild = new Form1();
+            newMDIChild.MdiParent = this;
+            newMDIChild.Show();
+        }
+    }
+}
